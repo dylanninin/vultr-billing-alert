0d62a9a3405d3fdb8d3feb50fd10b9c8e8db26f1
Dylan <dylanninin@gmail.com>
2021-11-22 23:31:55 +0800

    fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,13 @@ cd ${DIR}
 export VULTR_API_ACCOUNT="${VULTR_INPUT_API_ACCOUNT}"
 export VULTR_API_KEY="${INPUT_VULTR_API_KEY}"
 
-./vultr/main.yml
+/vultr/main.yml
 
-path=./vultr/billing_alert
+path=/vultr/billing_alert
 
 curl -X POST -H --silent --data "{\"text\": \"$(cat ${path} | sed "s/\"/'/g")\"}" "${INPUT_SLACK_WEBHOOK}"
 
-traffic=$(cat ./vultr/billing_alert | grep traffic | cut -d ':' -f2 | tr -d ' ')
-charge=$(cat ./vultr/billing_alert | grep charge | cut -d ':' -f2 | tr -d ' ')
+traffic=$(cat /vultr/billing_alert | grep traffic | cut -d ':' -f2 | tr -d ' ')
+charge=$(cat /vultr/billing_alert | grep charge | cut -d ':' -f2 | tr -d ' ')
 echo "::set-output name=vultr_traffic::${traffic}"
 echo "::set-output name=vultr_charge::${charge}"
